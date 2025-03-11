a5a19c388beb586ce74180314ed931e305ddb45a
915-Nicola-Razvan-Cristian <razvannicola451@gmail.com>
2025-03-11 13:58:44 +0200

    1

---
+++ MPP-app/src/Components/Title.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Title() {
+
+    return (
+        <div className="title-container">
+            <h1 className="title">Title</h1>
+        </div>
+    )
+}
